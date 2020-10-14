01b4bffa611afca7415b0d6ad3c56bceb673ff77
jpassgo <jeffpascoe9@gmail.com>
2020-10-13 21:28:29 -0500

    Properly defining urls and views.

--- PostsRestApi/urls.py
+++ PostsRestApi/urls.py
@@ -1,14 +1,11 @@
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'^$', views.home)
-router.register(r'^create_post/$', views.create_post)
-router.register(r'^delete_post/$', views.delete_post)
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', views.home),
+    path('create_post/', views.create_post),
+    path('delete_post/', views.delete_post),
     path('api-auth/', include('rest_framework.urls',
-        namespace='rest_framework'))
+         namespace='rest_framework'))
 ]
