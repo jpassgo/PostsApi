654cea9daca7d1552e37d98282b3b861d9ae0ab1
jpassgo <jeffpascoe9@gmail.com>
2020-10-14 21:26:36 -0500

    Updating views attempting to take in json request and create a new post.

--- PostsRestApi/views.py
+++ PostsRestApi/views.py
@@ -1,21 +1,37 @@
 import json
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def home(request):
+    author = request.GET.get('author')
+    print(author)
+    text = request.GET.get('text')
+    print(text)
     return HttpResponse(
-        json.dumps({{"PlaceHolderKey": "PlaceHolderValue"}}),
+        json.dumps({author: text}),
         content_type="application/json"
     )
 
+@csrf_exempt
 def create_post(request):
+    author = request.GET.get('author')
+    print(author)
+    text = request.GET.get('text')
+    print(text)
     return HttpResponse(
-        json.dumps({"PlaceHolderKey": "PlaceHolderValue"}),
+        json.dumps({author: text}),
         content_type="application/json"
     )
 
+@csrf_exempt
 def delete_post(request):
+    author = request.GET.get('author')
+    print(author)
+    text = request.GET.get('text')
+    print(text)
     return HttpResponse(
-        json.dumps({"PlaceHolderKey": "PlaceHolderValue"}),
+        json.dumps({author: text}),
         content_type="application/json"
     )
